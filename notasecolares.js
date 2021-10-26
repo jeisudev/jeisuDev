4fc691384c14c1eeb7206ad8fd32a397d9c8d256
Jeison Rothmund <92221656+jeisucs@users.noreply.github.com>
2021-10-26 00:37:07 -0300

    Add files via upload

---
+++ notasecolares.js
@@ -0,0 +1,26 @@
+// calculo de notas ecolares
+function somar(score) {
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 80 && score <= 89
+    let scoreC = score >= 70 && score <= 79
+    let scoreD = score >= 60 && score <= 69
+    let scoreF = score < 60 && score >= 0
+
+    let scoreFinal;
+
+    if(scoreA) {
+        scoreFinal = "A"
+    } else if (scoreB) {
+        scoreFinal = "B"
+    }else if (scoreC) {
+        scoreFinal = "C"
+    }else if (scoreD) {
+        scoreFinal = "D"
+    }else if (scoreF) {
+        scoreFinal = "F"
+    } else {
+        scoreFinal = "not invalid"
+    }
+    console.log(scoreFinal)
+   }
+   somar(75)
